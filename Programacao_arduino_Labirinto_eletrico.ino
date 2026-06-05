8c8f87438c42a0fc258110dd945c9151de050c13
DiegoTarcisio <diegotarcisiomacedo@gmail.com>
2026-06-04 22:04:26 -0300

    Refactor setup and loop functions for input handling
    Updated pin modes to use INPUT_PULLUP and adjusted logic for reading states.

--- Programacao_arduino_Labirinto_eletrico.ino
+++ Programacao_arduino_Labirinto_eletrico.ino
@@ -6,10 +6,11 @@
 #define buzzer 0
 int estado = LOW;
 
-void setup() {
-  pinMode(inicio, INPUT);
-  pinMode(arame, INPUT);
-  pinMode(fim, INPUT);
+
+void setup( ) {
+  pinMode(inicio, INPUT_PULLUP);
+  pinMode(arame, INPUT_PULLUP);
+  pinMode(fim, INPUT_PULLUP);
   pinMode(ledverm, OUTPUT);
   pinMode(ledverd, OUTPUT);
   pinMode(buzzer, OUTPUT);
@@ -18,31 +19,31 @@ void setup() {
   digitalWrite(ledverd, LOW);
 }
 
-void loop() {
-  if (digitalRead(arame) == HIGH && estado == HIGH) {
+void loop( ) {
+  if(digitalRead(arame) == LOW && estado == LOW){
     digitalWrite(ledverm, HIGH);
     tone(buzzer, 1109);
     delay(1000);
     noTone(buzzer);
-    estado = LOW;
-  }
-
-  if (digitalRead(inicio) == HIGH) {
     estado = HIGH;
+  }
+  
+  if(digitalRead(inicio) == LOW){
+    estado = LOW;
     digitalWrite(ledverm, LOW);
     digitalWrite(ledverd, HIGH);
     delay(500);
     digitalWrite(ledverd, LOW);
   }
-
-  if (digitalRead(fim) == HIGH && estado == HIGH) {
+  
+  if(digitalRead(fim) == LOW && estado == LOW){
     digitalWrite(ledverd, HIGH);
     tone(buzzer, 880, 150);
     delay(300);
     tone(buzzer, 1109, 150);
     delay(300);
     tone(buzzer, 1175, 150);
-    estado = LOW;
+    estado = HIGH;
   }
   delay(10);
 }
